ac5e0a710e6608c5951ae4f373e2715e9dcf796f
amjad78678 <81846358+amjad78678@users.noreply.github.com>
2024-06-21 00:06:38 +0530

    testing banner

--- middleware/uploadImages.js
+++ middleware/uploadImages.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const storage = multer.diskStorage({ 
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '..', 'public', 'assetsAdmin', 'imgs', 'products'));
+    cb(null, path.join(__dirname, '..', 'public', 'assetsAdmin', 'imgs'));
   },
   filename: (req, file, cb) => {
     const name = Date.now() + '-' + file.originalname;
