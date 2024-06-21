f35b4309571180d2a0b0b564e0105062020af2d9
amjad78678 <81846358+amjad78678@users.noreply.github.com>
2024-06-21 14:39:58 +0530

    after remove static file because i givened in nginx

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   console.log('Requested file path:', path.join(__dirname, req.url));
   next();
 });
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));  
 
 const userRouter = require('./routes/userRoute');
 app.use('/', userRouter);
@@ -48,4 +48,4 @@ app.listen(port, () =>
   console.log(`port is running at http://localhost:${port}/  port is running at http://localhost:${port}/admin`),
 );
 
-// hello
+// hello bruda endokkeya
