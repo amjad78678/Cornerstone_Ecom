c6853ce418fad82892248fa8b40b6cd18496dd6e
amjad78678 <81846358+amjad78678@users.noreply.github.com>
2024-06-21 14:44:15 +0530

    changed static now

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   console.log('Requested file path:', path.join(__dirname, req.url));
   next();
 });
-// app.use(express.static(path.join(__dirname, 'public')));  
+app.use(express.static(path.join(__dirname, 'public')));  
 
 const userRouter = require('./routes/userRoute');
 app.use('/', userRouter);
